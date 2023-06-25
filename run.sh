bdb2dfa78b9abb533a37ff2a5d4d01c9177e21da
Luke Chadwick <luke@vertis.io>
2023-06-25 16:55:34 +0100

    Add script to run

---
+++ run.sh
@@ -0,0 +1,9 @@
+cd redis
+bash gen-redis-cert.sh
+cd ../
+
+# using the base image layer in the redis/tls cert directory
+docker build -t example/redis:v7.0.4 -f ./redis/Dockerfile ./
+
+# start redis with ssl see: docker-redis-entrypoint.sh
+docker-compose up -d
